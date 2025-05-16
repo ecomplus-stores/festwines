1bced6144961790a53322161bc186ba856b661b7
Eduardo <edu.vlemes@gmail.com>
2025-05-16 18:06:54 -0300

    .

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -8,7 +8,12 @@ module.exports = () => ({
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/components/TheProduct.html'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/components/ProductGallery.html'),
       './js/DiscountApplier.js': path.resolve(__dirname, 'template/js/components/DiscountApplier.js'),
-      './../lib/fix-item-final-price': path.resolve(__dirname, 'template/js/custom-js/fix-item-final-price.js')
+      './../lib/fix-item-final-price': path.resolve(__dirname, 'template/js/custom-js/fix-item-final-price.js'),
+
+
+      './html/ProductCard.html': path.resolve(__dirname, 'template/alpix/html/ProductCard.html'),
+      //'./js/ProductCard.js': path.resolve(__dirname, 'template/alpix/js/ProductCard.js'),
+      // '../../storefront-twbs/scss/_reboot.scss': path.resolve(__dirname, 'template/alpix/reboot.scss'),
     }
   }
 })
